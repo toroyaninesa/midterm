9ad4ba7d8b0798fe6a508a5e5b08110e73957d60
Inesa Toroyan <inesatoroyan@Inesas-MacBook-Pro.local>
2023-10-20 10:07:56 +0400

    adding game title to the view

--- app/src/main/java/com/example/testapplication/GameScreen.kt
+++ app/src/main/java/com/example/testapplication/GameScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -30,6 +29,11 @@ fun GameScreen() {
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Bull's Eye Game",
+            color = Color.Black
+        )
+
         Text(
             text = "Target Value: $targetValue",
             color = Color.Black
